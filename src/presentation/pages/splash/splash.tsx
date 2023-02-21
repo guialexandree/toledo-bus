7611ac34689f2e2158c425e8772aceee348a2ca9
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-02-21 12:17:03 -0300

    feat: adiciona spinner na splash screen

--- src/presentation/pages/splash/splash.tsx
+++ src/presentation/pages/splash/splash.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import S from './splash-styles.scss'
-import { Icon, IconName, Logo } from '@/presentation/components'
+import { Icon, IconName, Logo, Spinner } from '@/presentation/components'
 
 const Splash: React.FC = () => {
   return (
     <section className={S.splashWrap}>
       <Logo />
       <Icon iconName={IconName.busLogin} />
+      <Spinner />
     </section>
   )
 }
