089f75b8e746b03ae0d3cd05e3844339ba7ac8dc
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-02-21 12:16:38 -0300

    feat: adiciona component radioButton

---
+++ src/presentation/components/radio-button/radio-button.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import S from './radio-button-styles.scss'
+
+type RadioButtonProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & {
+  id: string
+  label: string
+  checked?: boolean
+  group?: string
+  icon?: React.ReactNode
+}
+
+const RadioButton: React.FC<RadioButtonProps> = ({
+  id,
+  label,
+  checked = false,
+  group,
+  icon,
+  ...props
+}) => {
+  return (
+    <section className={S.radioButtonWrap}>
+      <label htmlFor={id} className={checked && S.active}>
+        {icon}
+        {label}
+        <input
+          type="radio"
+          id={id}
+          name={group || id}
+          {...props}
+        />
+      </label>
+    </section>
+  )
+}
+
+export default RadioButton
