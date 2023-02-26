2030189ba4194872687f79b5711cfb75718ea3d8
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-02-26 14:58:22 -0300

    refactor: refatoração da tela splash

--- src/presentation/pages/splash/components/splash-animations.ts
+++ src/presentation/pages/splash/components/splash-animations.ts
@@ -4,15 +4,25 @@ export const logoAnimations = {
     y: 0,
     opacity: 1,
     transition: {
-      duration: 0.8,
-      delay: 1
+      duration: 0.4,
+      delay: 1.3,
+      type: 'spring',
+      damping: 8
+    }
+  },
+  exit: {
+    y: 0,
+    opacity: 1,
+    scale: 0,
+    transition: {
+      duration: 0.3
     }
   }
 }
 
-export const busAnimations: any = {
+export const busRouteAnimations: any = {
   hidden: { x: -1080, opacity: 0 },
-  route1: {
+  route: {
     x: 1,
     opacity: 1,
     transition: {
@@ -23,7 +33,7 @@ export const busAnimations: any = {
       stiffness: 140
     }
   },
-  exit1: {
+  exit: {
     x: 2000,
     opacity: 1,
     transition: {
@@ -33,9 +43,6 @@ export const busAnimations: any = {
   },
   route2: {
     x: 24,
-    position: 'fixed',
-    bottom: -4,
-    left: 0,
     opacity: 1,
     scaleX: -1,
     transition: {
@@ -46,10 +53,38 @@ export const busAnimations: any = {
       stiffness: 150
     }
   },
-  route3: {
-    position: 'fixed',
+  exit2: {
     x: -200,
     opacity: 0
+  }
+}
+
+export const busRouteSecondAnimations: any = {
+  hidden: {
+    position: 'fixed',
+    bottom: -4,
+    left: 0,
+    x: 2000,
+    opacity: 0
+  },
+  route: {
+    x: 24,
+    opacity: 1,
+    scaleX: -1,
+    transition: {
+      delay: 0.2,
+      type: 'spring',
+      damping: 15,
+      mass: 0.55,
+      stiffness: 150
+    }
+  },
+  exit: {
+    x: -400,
+    opacity: 1,
+    transition: {
+      duration: 2
+    }
   }
 }
 
@@ -66,6 +101,7 @@ export const peopleAnimations = {
     opacity: 1
   },
   exit: {
+    scale: 0,
     opacity: 0
   }
 }
--- src/presentation/pages/splash/splash.tsx
+++ src/presentation/pages/splash/splash.tsx
@@ -10,15 +10,17 @@ const Splash: React.FC = () => {
   const peopleContol = useAnimationControls()
   const logoControl = useAnimationControls()
   const busControl = useAnimationControls()
+  const busControlSecond = useAnimationControls()
 
   const sequence = async (): Promise<void> => {
     await peopleContol.start('visible')
     await logoControl.start('visible')
-    await busControl.start('route1')
-    await busControl.start('exit1')
-    await busControl.start('route2')
+    await busControl.start('route')
+    await busControl.start('exit')
+    await busControlSecond.start('route')
     await peopleContol.start('exit')
-    await busControl.start('route3')
+    await busControlSecond.start('exit')
+    await logoControl.start('exit')
     Promise.resolve()
   }
 
@@ -39,7 +41,7 @@ const Splash: React.FC = () => {
         <Animated
           className={S.busIcon}
           control={busControl}
-          variants={Animations.busAnimations}
+          variants={Animations.busRouteAnimations}
         >
           <Icon iconName={IconName.busLogin} />
         </Animated>
@@ -48,6 +50,13 @@ const Splash: React.FC = () => {
           variants={Animations.peopleAnimations}
         >
           <Icon iconName={IconName.peopleAwaitingBus} />
+        </Animated>
+        <Animated
+          className={S.busIconSecond}
+          control={busControlSecond}
+          variants={Animations.busRouteSecondAnimations}
+        >
+          <Icon iconName={IconName.busLogin} />
         </Animated>
       </section>
     </section>
