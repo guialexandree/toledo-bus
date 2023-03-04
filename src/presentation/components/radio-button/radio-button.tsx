816fbe0c873dfd2c9b8ae88afb5588bd97d0575e
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-03-04 17:31:43 -0300

    refactor: ajuste para receber estado do recoil

--- src/presentation/components/radio-button/radio-button.tsx
+++ src/presentation/components/radio-button/radio-button.tsx
@@ -2,30 +2,37 @@ import React from 'react'
 import S from './radio-button-styles.scss'
 
 type RadioButtonProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & {
-  id: string
+  name: string
   label: string
-  checked?: boolean
-  group?: string
+  checked: boolean
+  group: string
   icon?: React.ReactNode
+  setState: any
 }
 
 const RadioButton: React.FC<RadioButtonProps> = ({
-  id,
+  name,
   label,
   checked = false,
   group,
   icon,
+  setState,
   ...props
 }) => {
+  const handleChecked = (event: React.MouseEvent<HTMLInputElement>): void => {
+    event.preventDefault()
+    setState(old => ({ ...old, [group]: name }))
+  }
+
   return (
-    <section className={S.radioButtonWrap}>
-      <label htmlFor={id} data-status={checked}>
+    <section onClick={handleChecked} className={S.radioButtonWrap}>
+      <label htmlFor={name} data-status={checked}>
         {icon}
         {label}
         <input
           type="radio"
-          id={id}
-          name={group || id}
+          id={name}
+          name={group}
           {...props}
         />
       </label>
