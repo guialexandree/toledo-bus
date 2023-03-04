0e8adff46f68363029f714f65d3462e0559d1f96
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-03-04 17:36:06 -0300

    feat: desmembra formulário em novo component

--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -1,99 +1,23 @@
-import React, { useEffect, useState } from 'react'
-import { ButtonBase, FormBase, Header, RadioButton, Select, SubmitButtonBase, currentAccountState } from '@/presentation/components'
-import { Calendar } from 'phosphor-react'
-import S from './home-styles.scss'
+import React, { useEffect } from 'react'
 import { useRecoilState } from 'recoil'
-import { useMediaQuery } from '@/presentation/hooks'
+import { Header, currentAccountState } from '@/presentation/components'
+import { FormLines } from './components'
+import S from './home-styles.scss'
 
 type HomeProps = { }
 
-type DayFilter = 'today' | 'tomorrow' | 'period'
-
-const lines = [
-  '006 - Novo Sarandi',
-  '006 - Vila Nova',
-  '006 - Novo Sobradinho',
-  '007 - Ouro Preto',
-  '007 - Boa Vista',
-  '010 - Bom Principio',
-  '010 - Gramado',
-  '010 - São Luiz do Oeste',
-  '011 - Dois Irmãos',
-  '019 - Biopark',
-  '020 - Santa Clara IV',
-  '021 - Santa Clara II',
-  '022 - JD. Concórdia',
-  '023 - JD. Coopagro',
-  '024 - JD. São Francisco',
-  '025 - PUC',
-  '026 - Operária',
-  '027 - JD. Porto Alegre',
-  '028 - Vila Industrial',
-  '030 - Colonia',
-  '031 - Pioneira',
-  '032 - Europa Via Centro',
-  '034 - Panorama',
-  '048 - Metropolitano Toledo',
-  '291 - Europa Via Concórdia'
-]
-
 const Home: React.FC<HomeProps> = () => {
   const [currentAccount, setCurrentAccount] = useRecoilState(currentAccountState)
-  const [dayFilter, setDayFilter] = useState<DayFilter>('today')
-  const { isMobile } = useMediaQuery()
-
-  const handleDayFilter = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    event.preventDefault()
-    setDayFilter(event.target.id as DayFilter)
-  }
 
   useEffect(() => {
     const account = currentAccount.getCurrentAccount()
-    console.log('isMobile', isMobile())
   })
 
   return (
     <section className={S.homeWrap}>
       <section className={S.appBar}>
         <Header />
-        <FormBase className={S.filtersSearch} >
-          <fieldset>
-            <Select.Select
-              items={lines}
-              label='escolha a linha de ônibus'
-            />
-          </fieldset>
-          <fieldset data-list>
-            <RadioButton
-              id='today'
-              label='hoje'
-              group='day'
-              checked={dayFilter === 'today'}
-              onChange={handleDayFilter}
-            />
-            <RadioButton
-              id='tomorrow'
-              label='amanhã'
-              group='day'
-              checked={dayFilter === 'tomorrow'}
-              onChange={handleDayFilter}
-            />
-            <RadioButton
-              id='period'
-              label='data'
-              group='day'
-              icon={<Calendar size={16} />}
-              checked={dayFilter === 'period'}
-              onChange={handleDayFilter}
-            />
-          </fieldset>
-          <fieldset>
-            <SubmitButtonBase
-              text="visualizar horários"
-              state={{}}
-            />
-          </fieldset>
-        </FormBase>
+        <FormLines className={S.filtersSearch} />
         <section className={S.lastSearchsWrap}>
           <h2>últimas buscas</h2>
           <ul className={S.listSearchs}>
